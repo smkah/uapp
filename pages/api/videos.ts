13b1f3c978fb9ef6f1449bb9ba694196071e6f2b
smkah <samukahweb@gmail.com>
2022-06-14 18:05:03 -0300

    Change theme | convert to typescript

--- pages/api/videos.ts
+++ pages/api/videos.ts
@@ -1,25 +1,7 @@
 import * as yt from 'youtube-search-without-api-key';
-import Cors from 'cors'
 import type { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
-
-// Initializing the cors middleware
-const cors = Cors({
-  methods: ['GET', 'POST', 'HEAD'],
-})
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req: NextRequest, res: NextResponse, fn: any) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-
-      return resolve(result)
-    })
-  })
-}
+import runMiddleware from './_middleware';
+import cors from 'cors';
 
 export default async function videos(req: NextRequest | any, res: NextResponse | any) {
 
