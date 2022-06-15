ac37326097355074dea853d03b44bee74118c649
smkah <samukahweb@gmail.com>
2022-06-15 08:34:37 -0300

    Update Videos APIt

--- pages/api/videos.ts
+++ pages/api/videos.ts
@@ -1,9 +1,9 @@
 import * as yt from 'youtube-search-without-api-key';
-import type { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 import runMiddleware from './_middleware';
+import type { NextApiRequest, NextApiResponse } from 'next'
 import cors from 'cors';
 
-export default async function videos(req: NextRequest | any, res: NextResponse | any) {
+export default async function videos(req: NextApiRequest | any, res: NextApiResponse | any) {
 
   await runMiddleware(req, res, cors)
 
