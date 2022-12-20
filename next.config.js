e2bcd14c1e123a357cd1f1ea849b061f5854ab4d
smkah <samukahweb@gmail.com>
2022-12-20 17:54:17 -0300

    Initial PWA

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  // disable: true,
+  // disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+})
 
 module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-  },
   reactStrictMode: true,
-  images: {
-    domains: ['i.ytimg.com'],
-    loader: "imgix",
-    path: "",
-  }
+  // images: {
+  //   domains: ['i.ytimg.com'],
+  //   loader: "imgix",
+  //   path: "",
+  // }
 })
+
 
